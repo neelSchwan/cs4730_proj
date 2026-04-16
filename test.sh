b77569116c77fcedcaa0ba18fbbef01a93ca12b8
Neel Sawant <sawant.ne@northeastern.edu>
2026-04-16 14:29:18 -0400

    test script via bash

---
+++ test.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+set -e
+
+N1="10.200.125.71:12000"
+N2="10.200.125.72:12000"
+N3="10.200.125.73:12000"
+N4="10.200.125.74:12000"
+N5="10.200.125.75:12000"
+
+echo "--- registering user ---"
+curl -s -X POST http://$N1/users -H "Content-Type: application/json" -d '{"username":"grader"}'
+echo ""
+
+echo "--- posting review to node 1 ---"
+curl -s -X POST http://$N1/reviews -H "Content-Type: application/json" -d '{"user_id":1,"rating":5,"subject":"Gossip Test","description":"testing propagation"}'
+echo ""
+
+echo "--- waiting 3s for gossip to propagate ---"
+sleep 3
+
+echo "--- checking all nodes for review ---"
+echo "node 1:"; curl -s http://$N1/reviews; echo ""
+echo "node 2:"; curl -s http://$N2/reviews; echo ""
+echo "node 3:"; curl -s http://$N3/reviews; echo ""
+echo "node 4:"; curl -s http://$N4/reviews; echo ""
+echo "node 5:"; curl -s http://$N5/reviews; echo ""
+
+echo "--- posting review with fail-rate=0.9 on node 2 ---"
+echo "(restart node 2 with --fail-rate=0.9, then press enter)"
+read
+
+curl -s -X POST http://$N2/reviews -H "Content-Type: application/json" -d '{"user_id":1,"rating":3,"subject":"Fail Rate Test","description":"testing with high drop rate"}'
+echo ""
+
+echo "--- waiting 5s ---"
+sleep 5
+
+echo "--- checking propagation under failure ---"
+echo "node 1:"; curl -s http://$N1/reviews; echo ""
+echo "node 2:"; curl -s http://$N2/reviews; echo ""
+echo "node 3:"; curl -s http://$N3/reviews; echo ""
+echo "node 4:"; curl -s http://$N4/reviews; echo ""
+echo "node 5:"; curl -s http://$N5/reviews; echo ""
